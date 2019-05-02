58f412d9b5797067d81ab11e9bb0c6a6bd6b8008
Bohdan <qwebeck5@gmail.com>
2019-05-02 19:10:30 +0200

    Added default icons for documents and images , icons for applications .To fix : memory crush , save settings , design , code , shell script execution

--- build-Vass_2-Desktop-Debug/ui_icon_panel.h
+++ build-Vass_2-Desktop-Debug/ui_icon_panel.h
@@ -14,6 +14,7 @@
 #include <QtWidgets/QApplication>
 #include <QtWidgets/QButtonGroup>
 #include <QtWidgets/QHeaderView>
+#include <QtWidgets/QLabel>
 #include <QtWidgets/QPushButton>
 #include <QtWidgets/QWidget>
 
@@ -31,72 +32,123 @@ public:
     QPushButton *app_6;
     QPushButton *app_7;
     QPushButton *app_8;
+    QLabel *label_2;
+    QLabel *label_3;
+    QLabel *label_7;
+    QLabel *label_8;
+    QLabel *label_6;
+    QLabel *label_5;
+    QLabel *label_4;
+    QLabel *label_1;
+    QButtonGroup *application_buttons;
 
     void setupUi(QWidget *icon_panel)
     {
         if (icon_panel->objectName().isEmpty())
             icon_panel->setObjectName(QStringLiteral("icon_panel"));
-        icon_panel->resize(300, 150);
+        icon_panel->resize(300, 270);
         addShortCut = new QPushButton(icon_panel);
         addShortCut->setObjectName(QStringLiteral("addShortCut"));
-        addShortCut->setGeometry(QRect(100, 50, 100, 50));
+        addShortCut->setGeometry(QRect(100, 110, 100, 50));
         addShortCut->setStyleSheet(QStringLiteral("QPushButton { background-color: rgb(255, 151, 57); color: white; border: 1px solid gray; border-radius:10px} QPushButton:pressed { background-color: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1, stop: 0 #FF7832, stop: 1 #FF9739); }"));
         app_1 = new QPushButton(icon_panel);
+        application_buttons = new QButtonGroup(icon_panel);
+        application_buttons->setObjectName(QStringLiteral("application_buttons"));
+        application_buttons->addButton(app_1);
         app_1->setObjectName(QStringLiteral("app_1"));
-        app_1->setGeometry(QRect(0, 1, 100, 50));
-        app_1->setStyleSheet(QLatin1String("QPushButton { background-color: rgb(215, 215, 215); border: 1px solid gray;border-radius:10px } \n"
+        app_1->setGeometry(QRect(10, 0, 80, 90));
+        app_1->setStyleSheet(QLatin1String("QPushButton { background-color: rgb(215, 215, 215); border: 1px solid gray;border-radius:10px ; padding-top:40px} \n"
 "\n"
 "QPushButton:hover{ background-color: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1, stop: 0 rgb(115, 175, 215), stop: 1 rgb(175, 175, 215)); }\n"
 "QPushButton:pressed{ background-color: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1, stop: 0 #BEBEBE, stop: 1 #D7D7D7); }"));
         app_2 = new QPushButton(icon_panel);
+        application_buttons->addButton(app_2);
         app_2->setObjectName(QStringLiteral("app_2"));
-        app_2->setGeometry(QRect(100, 0, 100, 50));
-        app_2->setStyleSheet(QLatin1String("QPushButton { background-color: rgb(215, 215, 215); border: 1px solid gray;border-radius:10px } \n"
+        app_2->setGeometry(QRect(110, 0, 80, 90));
+        app_2->setStyleSheet(QLatin1String("QPushButton { background-color: rgb(215, 215, 215); border: 1px solid gray;border-radius:10px ; padding-top:40px} \n"
 "\n"
 "QPushButton:hover{ background-color: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1, stop: 0 rgb(115, 175, 215), stop: 1 rgb(175, 175, 215)); }\n"
 "QPushButton:pressed{ background-color: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1, stop: 0 #BEBEBE, stop: 1 #D7D7D7); }"));
         app_3 = new QPushButton(icon_panel);
+        application_buttons->addButton(app_3);
         app_3->setObjectName(QStringLiteral("app_3"));
-        app_3->setGeometry(QRect(200, 0, 100, 50));
-        app_3->setStyleSheet(QLatin1String("QPushButton { background-color: rgb(215, 215, 215); border: 1px solid gray;border-radius:10px } \n"
+        app_3->setGeometry(QRect(210, 0, 80, 90));
+        app_3->setStyleSheet(QLatin1String("QPushButton { background-color: rgb(215, 215, 215); border: 1px solid gray;border-radius:10px ; padding-top:40px} \n"
 "\n"
 "QPushButton:hover{ background-color: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1, stop: 0 rgb(115, 175, 215), stop: 1 rgb(175, 175, 215)); }\n"
 "QPushButton:pressed{ background-color: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1, stop: 0 #BEBEBE, stop: 1 #D7D7D7); }"));
         app_4 = new QPushButton(icon_panel);
+        application_buttons->addButton(app_4);
         app_4->setObjectName(QStringLiteral("app_4"));
-        app_4->setGeometry(QRect(0, 50, 100, 50));
-        app_4->setStyleSheet(QLatin1String("QPushButton { background-color: rgb(215, 215, 215); border: 1px solid gray;border-radius:10px } \n"
+        app_4->setGeometry(QRect(10, 90, 80, 90));
+        app_4->setStyleSheet(QLatin1String("QPushButton { background-color: rgb(215, 215, 215); border: 1px solid gray;border-radius:10px ; padding-top:40px} \n"
 "\n"
 "QPushButton:hover{ background-color: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1, stop: 0 rgb(115, 175, 215), stop: 1 rgb(175, 175, 215)); }\n"
 "QPushButton:pressed{ background-color: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1, stop: 0 #BEBEBE, stop: 1 #D7D7D7); }"));
         app_5 = new QPushButton(icon_panel);
+        application_buttons->addButton(app_5);
         app_5->setObjectName(QStringLiteral("app_5"));
-        app_5->setGeometry(QRect(200, 50, 100, 50));
-        app_5->setStyleSheet(QLatin1String("QPushButton { background-color: rgb(215, 215, 215); border: 1px solid gray;border-radius:10px } \n"
+        app_5->setGeometry(QRect(210, 90, 80, 90));
+        app_5->setStyleSheet(QLatin1String("QPushButton { background-color: rgb(215, 215, 215); border: 1px solid gray;border-radius:10px ; padding-top:40px} \n"
 "\n"
 "QPushButton:hover{ background-color: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1, stop: 0 rgb(115, 175, 215), stop: 1 rgb(175, 175, 215)); }\n"
 "QPushButton:pressed{ background-color: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1, stop: 0 #BEBEBE, stop: 1 #D7D7D7); }"));
         app_6 = new QPushButton(icon_panel);
+        application_buttons->addButton(app_6);
         app_6->setObjectName(QStringLiteral("app_6"));
-        app_6->setGeometry(QRect(0, 100, 100, 50));
-        app_6->setStyleSheet(QLatin1String("QPushButton { background-color: rgb(215, 215, 215); border: 1px solid gray;border-radius:10px } \n"
+        app_6->setGeometry(QRect(10, 180, 80, 90));
+        app_6->setStyleSheet(QLatin1String("QPushButton { background-color: rgb(215, 215, 215); border: 1px solid gray;border-radius:10px ; padding-top:40px} \n"
 "\n"
 "QPushButton:hover{ background-color: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1, stop: 0 rgb(115, 175, 215), stop: 1 rgb(175, 175, 215)); }\n"
 "QPushButton:pressed{ background-color: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1, stop: 0 #BEBEBE, stop: 1 #D7D7D7); }"));
         app_7 = new QPushButton(icon_panel);
+        application_buttons->addButton(app_7);
         app_7->setObjectName(QStringLiteral("app_7"));
-        app_7->setGeometry(QRect(100, 100, 100, 50));
-        app_7->setStyleSheet(QLatin1String("QPushButton { background-color: rgb(215, 215, 215); border: 1px solid gray;border-radius:10px } \n"
+        app_7->setGeometry(QRect(110, 180, 80, 90));
+        app_7->setStyleSheet(QLatin1String("QPushButton { background-color: rgb(215, 215, 215); border: 1px solid gray;border-radius:10px ; padding-top:40px} \n"
 "\n"
 "QPushButton:hover{ background-color: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1, stop: 0 rgb(115, 175, 215), stop: 1 rgb(175, 175, 215)); }\n"
 "QPushButton:pressed{ background-color: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1, stop: 0 #BEBEBE, stop: 1 #D7D7D7); }"));
         app_8 = new QPushButton(icon_panel);
+        application_buttons->addButton(app_8);
         app_8->setObjectName(QStringLiteral("app_8"));
-        app_8->setGeometry(QRect(200, 100, 100, 50));
-        app_8->setStyleSheet(QLatin1String("QPushButton { background-color: rgb(215, 215, 215); border: 1px solid gray;border-radius:10px } \n"
+        app_8->setGeometry(QRect(210, 180, 80, 90));
+        app_8->setStyleSheet(QLatin1String("QPushButton { background-color: rgb(215, 215, 215); border: 1px solid gray;border-radius:10px ; padding-top:40px} \n"
 "\n"
 "QPushButton:hover{ background-color: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1, stop: 0 rgb(115, 175, 215), stop: 1 rgb(175, 175, 215)); }\n"
 "QPushButton:pressed{ background-color: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1, stop: 0 #BEBEBE, stop: 1 #D7D7D7); }"));
+        label_2 = new QLabel(icon_panel);
+        label_2->setObjectName(QStringLiteral("label_2"));
+        label_2->setGeometry(QRect(120, 10, 51, 41));
+        label_2->setFrameShadow(QFrame::Raised);
+        label_3 = new QLabel(icon_panel);
+        label_3->setObjectName(QStringLiteral("label_3"));
+        label_3->setGeometry(QRect(220, 0, 61, 50));
+        label_3->setFrameShadow(QFrame::Raised);
+        label_7 = new QLabel(icon_panel);
+        label_7->setObjectName(QStringLiteral("label_7"));
+        label_7->setGeometry(QRect(120, 180, 61, 50));
+        label_7->setFrameShadow(QFrame::Raised);
+        label_8 = new QLabel(icon_panel);
+        label_8->setObjectName(QStringLiteral("label_8"));
+        label_8->setGeometry(QRect(220, 180, 51, 50));
+        label_8->setFrameShadow(QFrame::Raised);
+        label_6 = new QLabel(icon_panel);
+        label_6->setObjectName(QStringLiteral("label_6"));
+        label_6->setGeometry(QRect(20, 180, 61, 50));
+        label_6->setFrameShadow(QFrame::Raised);
+        label_5 = new QLabel(icon_panel);
+        label_5->setObjectName(QStringLiteral("label_5"));
+        label_5->setGeometry(QRect(220, 90, 61, 50));
+        label_5->setFrameShadow(QFrame::Raised);
+        label_4 = new QLabel(icon_panel);
+        label_4->setObjectName(QStringLiteral("label_4"));
+        label_4->setGeometry(QRect(19, 90, 61, 50));
+        label_4->setFrameShadow(QFrame::Raised);
+        label_1 = new QLabel(icon_panel);
+        label_1->setObjectName(QStringLiteral("label_1"));
+        label_1->setGeometry(QRect(20, 0, 61, 61));
+        label_1->setFrameShadow(QFrame::Raised);
 
         retranslateUi(icon_panel);
 
@@ -115,6 +167,14 @@ public:
         app_6->setText(QApplication::translate("icon_panel", "PushButton", Q_NULLPTR));
         app_7->setText(QApplication::translate("icon_panel", "PushButton", Q_NULLPTR));
         app_8->setText(QApplication::translate("icon_panel", "PushButton", Q_NULLPTR));
+        label_2->setText(QString());
+        label_3->setText(QApplication::translate("icon_panel", "TextLabel", Q_NULLPTR));
+        label_7->setText(QApplication::translate("icon_panel", "TextLabel", Q_NULLPTR));
+        label_8->setText(QString());
+        label_6->setText(QApplication::translate("icon_panel", "TextLabel", Q_NULLPTR));
+        label_5->setText(QApplication::translate("icon_panel", "TextLabel", Q_NULLPTR));
+        label_4->setText(QApplication::translate("icon_panel", "TextLabel", Q_NULLPTR));
+        label_1->setText(QApplication::translate("icon_panel", "TextLabel", Q_NULLPTR));
     } // retranslateUi
 
 };
