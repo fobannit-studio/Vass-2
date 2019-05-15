cf12a8db21b51aa7ad3cc6939e1293468eea4c26
Bohdan <qwebeck5@gmail.com>
2019-05-15 23:26:02 +0200

    added shortcuts for all apps

--- Vass_2/event_filter.cpp
+++ Vass_2/event_filter.cpp
@@ -1,9 +1,5 @@
 #include "event_filter.h"
-#include <QVector>
-#include <QX11Info>
-#include <X11/Xlib.h>
-#include <X11/Xutil.h>
-#include <xcb/xcb.h>
+
 namespace
 {
     Display * m_display;
@@ -16,10 +12,15 @@ namespace
     }
 }
 
-event_filter::event_filter(QObject *parent):QObject (parent)
+event_filter::event_filter(KeySym icons , KeySym config , KeySym media , KeySym clock , QObject *parent):QObject (parent)
 {
     m_display = QX11Info::display();
     m_win = DefaultRootWindow(m_display);
+    _icon_keycode = XKeysymToKeycode(m_display,icons);
+    _config_keycode=XKeysymToKeycode(m_display,config);
+    _media_keycode = XKeysymToKeycode(m_display,media);
+    _clock_keycode = XKeysymToKeycode(m_display,clock);
+
 }
 
 bool event_filter::nativeEventFilter(const QByteArray &eventType, void *message, long *result)
@@ -36,9 +37,24 @@ bool event_filter::nativeEventFilter(const QByteArray &eventType, void *message,
             keyEvent = static_cast<xcb_key_press_event_t *>(message);
             foreach(quint32 maskMods , maskModifiers())
             {
-                if((keyEvent->state==(modifier | maskMods)) && keyEvent ->detail == keycode)
+                if((keyEvent->state==(modifier | maskMods)) && keyEvent ->detail == _icon_keycode)
                 {
-                    emit activated();
+                    emit icon_called();
+                    return true;
+                }
+                else if((keyEvent->state==(modifier | maskMods)) && keyEvent ->detail == _config_keycode)
+                {
+                    emit config_called();
+                    return true;
+                }
+                else if((keyEvent->state==(modifier | maskMods)) && keyEvent ->detail == _media_keycode)
+                {
+                    emit player_called();
+                    return true;
+                }
+                else if((keyEvent->state==(modifier | maskMods)) && keyEvent ->detail == _clock_keycode)
+                {
+                    emit clock_called();
                     return true;
                 }
             }
@@ -48,17 +64,35 @@ bool event_filter::nativeEventFilter(const QByteArray &eventType, void *message,
 }
 
 
-void event_filter::setShortcut()
+void event_filter::setShortcut( Apps application)
 {
-    unsetShortcut();
+    int key_code;
+    switch (application) {
+    case Apps::Icons:
+        key_code = _icon_keycode;
+        break;
+    case Apps::Config:
+        key_code = _config_keycode;
+        break;
+    case Apps::Player:
+        key_code = _media_keycode;
+        break;
+    case Apps::Clock:
+        key_code = _clock_keycode;
+        break;
+    default:
+        return;
+    }
+    unsetShortcut(key_code);
     //modify here
-    keycode = XKeysymToKeycode(m_display,XK_M);
+//    keycode = XKeysymToKeycode(m_display,key);
+
     modifier = ControlMask;
     //
     foreach(quint32 maskMods , maskModifiers())
     {
         XGrabKey(m_display,         // указываем соединение с X11
-                 keycode ,          // код клавиши
+                 key_code ,          // код клавиши
                  modifier | maskMods,   // модификатор со всеми возможными масками
                  m_win,             // Захватываемое окно
                  True,              // Является ли приложение владельцем события. в данном примере не принципиально.
@@ -67,11 +101,11 @@ void event_filter::setShortcut()
     }
 }
 
-void event_filter::unsetShortcut(){
+void event_filter::unsetShortcut(int key){
     foreach(quint32 maskMods,maskModifiers())
     {
         XUngrabKey(m_display,
-                   keycode,
+                   key,
                    modifier | maskMods,
                    m_win);
     }
--- Vass_2/event_filter.h
+++ Vass_2/event_filter.h
@@ -3,19 +3,34 @@
 #ifdef linux
 #include <QObject>
 #include <QAbstractNativeEventFilter>
+#include <QVector>
+#include <QX11Info>
+#include <X11/Xlib.h>
+#include <X11/Xutil.h>
+#include <xcb/xcb.h>
+enum class Apps{Player,Config,Icons,Clock};
 
 class event_filter: public QObject , public QAbstractNativeEventFilter
 {
     Q_OBJECT
 public:
-    explicit event_filter(QObject *parent = nullptr);
+    explicit event_filter(KeySym icons , KeySym config , KeySym param , KeySym clock , QObject *parent = nullptr);
 
     bool nativeEventFilter(const QByteArray &eventType , void *message ,long *result);
-    void setShortcut();
-    void unsetShortcut();
+//    void setShortcut(KeySym);
+    void setShortcut(Apps);
+    void unsetShortcut(int);
 
 signals:
-    void activated();
+    void icon_called();
+    void config_called();
+    void player_called();
+    void clock_called();
+private:
+    int _icon_keycode;
+    int _config_keycode;
+    int _media_keycode;
+    int _clock_keycode;
 };
 #endif
 #endif // EVENT_FILTER_H
